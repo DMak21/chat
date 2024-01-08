8d997f68c654b46adca7db30c8dddd1ce6d2952e
Deepesh Makhijani <deepmak@amazon.com>
2024-01-09 01:42:45 +0530

    Add Auth

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+// pages/_app.tsx
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import config from '@/amplifyconfiguration.json';
+import '@aws-amplify/ui-react/styles.css';
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
+
+Amplify.configure(config);
+
+function App({signOut, user }: WithAuthenticatorProps) {
+  return (
+    <>
+      <h1>Hello {user?.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
+  );
+}
+
+export default withAuthenticator(App);
